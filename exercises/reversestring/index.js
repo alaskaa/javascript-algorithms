0adb2c48b085d30d32fa9eaf4667eb23b9f1eb08
alaskaa <sibylle.sehl@googlemail.com>
2019-09-09 20:10:33 +0200

    [TASK] add an alternative solution without reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//function reverse(str) {
+//  return str.split("").reverse().join("");
+//}
+
 function reverse(str) {
-  return str.split("").reverse().join("");
+  const reversed = [];
+
+  str.split("").forEach(char => reversed.unshift(char));
+
+  return reversed.join("");
 }
 
 module.exports = reverse;
